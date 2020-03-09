a749202acd54be298030be72e2d2ff65c6c519f6
Manu <mcabrera@iaa.es>
2020-03-09 12:17:11 +0100

    Update addFitsHeader.sh

--- addFitsHeader.sh
+++ addFitsHeader.sh
@@ -24,13 +24,27 @@ if [[ $# -ne 5 ]]; then
     exit 2
 fi
 
-#
-# Recorre todo los ficheros en el directorio del parametro 1 y les añade las cabeceras necesarias
-#
-for i in $(ls -C1 $1)
-do
-    /home/mcabrera/MILOS/utilsFits/modhead $1/$i CTYPE1 $2
-    /home/mcabrera/MILOS/utilsFits/modhead $1/$i CTYPE2 $3
-    /home/mcabrera/MILOS/utilsFits/modhead $1/$i CTYPE3 $4
-    /home/mcabrera/MILOS/utilsFits/modhead $1/$i CTYPE4 $5
-done
+if [[ -d $1 ]]; then
+    echo "$1 is a directory"
+    #
+    # Browse all the files in the directory of parameter 1 and add the necessary headers
+    #
+    for i in $(ls -C1 $1)
+    do
+        /home/mcabrera/MILOS/utilsFits/modhead $1/$i CTYPE1 $2
+        /home/mcabrera/MILOS/utilsFits/modhead $1/$i CTYPE2 $3
+        /home/mcabrera/MILOS/utilsFits/modhead $1/$i CTYPE3 $4
+        /home/mcabrera/MILOS/utilsFits/modhead $1/$i CTYPE4 $5
+    done    
+elif [[ -f $1 ]]; then
+    echo "$1 is a file"
+    /home/mcabrera/MILOS/utilsFits/modhead $1 CTYPE1 $2
+    /home/mcabrera/MILOS/utilsFits/modhead $1 CTYPE2 $3
+    /home/mcabrera/MILOS/utilsFits/modhead $1 CTYPE3 $4
+    /home/mcabrera/MILOS/utilsFits/modhead $1 CTYPE4 $5    
+else
+    echo "$1 is not valid"
+    exit 1
+fi
+
+
